df1c789f2c8fa96bc13e96191fb55bbbb3d1556c
rizalfadilah00 <izalmarhas@gmail.com>
2024-09-19 10:51:16 +0700

    Update page2.dart

--- lib/page2.dart
+++ lib/page2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 
 class page2 extends StatelessWidget {
   const page2({super.key});
@@ -7,12 +9,41 @@ class page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        
-
-         child: Text('Pindah ke halaman 2'),
-      ),
-    );
-  }
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Row(
+         children: [
+            Container(  
+            height: 100,
+            width: 100,
+            color: Colors.red,
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          color: Colors.green,
+        )
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amber,
+            ),
+          ],
+        ),
+         ],
+          ),
+        ),
+      );
+    }
 }
-    
     
